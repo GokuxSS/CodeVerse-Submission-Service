e6aa0d996d7b09693e919c6305356525df4fdf38
jaykumar286 <jaykumarchavda2806@gmail.com>
2024-07-28 22:06:07 +0530

    added an sync comm with problem service and async with submission service

--- src/services/submissionService.js
+++ src/services/submissionService.js
@@ -1,5 +1,6 @@
 const SubmissionRepository = require('../repostitory/submissionRepository');
 const SubmissionProducer = require('../producers/submissionProducer');
+const { fetchProblemDetails } = require('../api/problemAdminApi');
 
 
 class SubmissionService{
@@ -8,12 +9,34 @@ class SubmissionService{
     }
 
     async addSubmission(submissionData){
-        const data = await this.submissionRepository.createSubmission(submissionData);
-        if(!data) {
+        const problemData = await fetchProblemDetails(submissionData.problemId);
+
+        if(!problemData) {
+            throw {messgae:'Failed to create a submission in the repository'};
+        }
+
+
+        const languageCodeStub = problemData.data.codeStubs.find(codeStub => codeStub.language.toLowerCase() === submissionData.language.toLowerCase());
+
+        console.log(languageCodeStub);
+
+        submissionData.code = languageCodeStub.startSnippet + "\n\n" + submissionData.code + "\n\n" + languageCodeStub.endSnippet;
+        
+        const submittedData = await this.submissionRepository.createSubmission(submissionData);
+        if(!submittedData) {
             // TODO: Add error handling here
             throw {messgae: "Not able to create submission"};
         }
-        const response = await SubmissionProducer(submissionData);
+
+
+        const response = await SubmissionProducer({
+            [submittedData._id]: {
+                code: submittedData.code,
+                language: submittedData.language,
+                inputCases: problemData.data.testCases[0].input,
+                outputCases: problemData.data.testCases[0].output,
+            }
+        });
         return {queueResponse: response, submissionData};
     }
 }
